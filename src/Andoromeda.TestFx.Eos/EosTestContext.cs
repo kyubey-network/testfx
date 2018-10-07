6802074a119bbe0f86e538fc549fc07bfd16b1fe
Yuko Zheng <1@1234.sh>
2018-10-07 22:58:11 +0800

    init

---
+++ src/Andoromeda.TestFx.Eos/EosTestContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Docker.DotNet;
+
+namespace Andoromeda.TestFx.Eos
+{
+    public class EosTestContext : IDisposable
+    {
+        private readonly DockerClient client;
+        EosTestContext()
+        {
+            string uri;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                uri = "//./pipe/docker_engine";
+            }
+            else
+            {
+                uri = "unix:/var/run/docker.sock";
+            }
+            var config = new DockerClientConfiguration(new Uri(uri));
+            client = config.CreateClient();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ src/Andoromeda.TestFx.Eos/ProcessExitedWithNonZeroException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Andoromeda.TestFx.Eos
+{
+    public class ProcessExitedWithNonZeroException : Exception
+    {
+        public ProcessExitedWithNonZeroException(int exitCode, string errorMessage)
+        {
+            this.ExitCode = exitCode;
+            this.ErrorMessage = errorMessage;
+        }
+
+        public int ExitCode { get; private set; }
+
+        public string ErrorMessage { get; private set; }
+    }
+}
